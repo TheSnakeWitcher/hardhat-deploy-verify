c23fb966a819ce7bf22df4ec85c673614f844b6e
Mr.PAPI <thesnakewitcher@gmail.com>
2023-09-24 11:25:40 -0400

    feat: add verify flag to deploy task

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { task, subtask } from "hardhat/config";
+import * as sources_names from "hardhat/utils/source-names"
+import * as contracts_names from "hardhat/utils/contract-names"
+
+
+const TASK_DEPLOY = "deploy" ;
+const TASK_DEPLOY_VERIFY = `${TASK_DEPLOY}:verify` ;
+
+
+task(TASK_DEPLOY)
+    .addFlag("verify", "automatically verify deployed contract after deploy")
+    .setAction(async function(args, hre, runSuper) {
+        const verifyDeployedContractTask = hre.tasks[TASK_DEPLOY_VERIFY].name;
+
+        await runSuper(args);
+
+        if (args.verify) {
+            await hre.run(verifyDeployedContractTask, args);
+        }
+    })
+
+
+subtask(TASK_DEPLOY_VERIFY, "automatically verify a contract deployed with hardhat-deploy", async function(args, hre) {
+    const verifyTask = hre.tasks.verify.name ;
+
+    const contract_name = args.tags ;
+    const contract_path = sources_names.localSourceNameToPath(hre.config.paths.root,contract_name);
+    const contract = contracts_names.getFullyQualifiedName(contract_path,contract_name);
+
+    const deployment = await hre.deployments.get(contract_name);
+    const address = deployment.address;
+    const constructorArgsParams = JSON.stringify(deployment.args)
+
+    await hre.run(verifyTask, { address, contract, constructorArgsParams });
+})
