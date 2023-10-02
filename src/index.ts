a51b4012a7641143f24e6b4dd888165b3b48bdee
Mr.PAPI <thesnakewitcher@gmail.com>
2023-10-02 14:53:03 -0400

    chore: rename main.ts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import * as sources_names from "hardhat/utils/source-names"
 import * as contracts_names from "hardhat/utils/contract-names"
 
 
-const TASK_VERIFY = "verify"  ;
-const TASK_DEPLOY = "deploy" ;
-const TASK_DEPLOY_VERIFY = `${TASK_DEPLOY}:${TASK_VERIFY}` ;
+const TASK_VERIFY = "verify";
+const TASK_DEPLOY = "deploy";
+const TASK_DEPLOY_VERIFY = `${TASK_DEPLOY}:${TASK_VERIFY}`;
 
 
 task(TASK_DEPLOY)
-    .addFlag("verify", "automatically verify deployed contract after deploy")
+    .addFlag("verify", "automatically verify deployed contract")
     .setAction(async function(args, hre, runSuper) {
         const verifyDeployedContractTask = hre.tasks[TASK_DEPLOY_VERIFY].name;
 
@@ -23,9 +23,9 @@ task(TASK_DEPLOY)
 
 subtask(TASK_DEPLOY_VERIFY, "automatically verify a contract deployed with hardhat-deploy", async function(args, hre) {
 
-    const contract_name = args.tags ;
-    const contract_path = sources_names.localSourceNameToPath(hre.config.paths.root,contract_name);
-    const contract = contracts_names.getFullyQualifiedName(contract_path,contract_name);
+    const contract_name = args.tags;
+    const contract_path = sources_names.localSourceNameToPath(hre.config.paths.root, contract_name);
+    const contract = contracts_names.getFullyQualifiedName(contract_path, contract_name);
 
     const deployment = await hre.deployments.get(contract_name);
     const address = deployment.address;
